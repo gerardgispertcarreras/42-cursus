c7dc165a73033b653290b7c6f632b4eaa0acbdd8
Gerard Gispert Carreras <ggispert@cbr9s4.42barcelona.com>
2023-07-29 19:00:03 +0200

    Libft update for next projects

---
+++ Libft/srcs/ft_itoa.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggispert <ggispert@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 17:30:02 by ggispert          #+#    #+#             */
+/*   Updated: 2023/05/01 12:27:06 by ggispert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+int	get_digits(int n)
+{
+	if (n / 10 == 0)
+		return (1);
+	return (get_digits(n / 10) + 1);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*a;
+	int		m;
+	int		sig;
+
+	m = get_digits(n);
+	if (n < 0)
+	{
+		sig = -1;
+		++m;
+	}
+	else
+		sig = 1;
+	a = malloc((m + 1) * sizeof(char));
+	if (!a)
+		return (NULL);
+	a[m] = '\0';
+	while (--m >= 0)
+	{
+		a[m] = (n % 10) * sig + '0';
+		n /= 10;
+	}
+	if (sig == -1)
+		a[0] = '-';
+	return (a);
+}
---
+++ Libft/srcs_bonus/ft_lstclear_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggispert <ggispert@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 18:16:22 by ggispert          #+#    #+#             */
+/*   Updated: 2023/04/29 12:34:16 by ggispert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (lst)
+	{
+		if (*lst != NULL)
+		{
+			ft_lstclear(&(*lst)->next, del);
+			ft_lstdelone(*lst, del);
+			*lst = NULL;
+		}
+	}
+}
