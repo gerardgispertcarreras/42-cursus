5c0963dee5d156a7d1c5c9ba344b9d099ec935f5
gerardgispert <gerard@amphoralogistics.com>
2023-08-30 12:49:20 +0200

    added push swap

---
+++ push-swap/Libft/srcs/ft_strjoin.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggispert <ggispert@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 19:56:09 by ggispert          #+#    #+#             */
+/*   Updated: 2023/04/29 12:34:16 by ggispert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*p;
+	size_t	len1;
+	size_t	len2;
+
+	if (s1)
+		len1 = ft_strlen(s1);
+	else
+		len1 = 0;
+	if (s2)
+		len2 = ft_strlen(s2);
+	else
+		len2 = 0;
+	p = malloc((len1 + len2 + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	ft_memmove(p, s1, len1);
+	ft_strlcpy(p + len1, s2, len2 + 1);
+	return (p);
+}
---
+++ push-swap/Libft/srcs/ft_strlcat.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggispert <ggispert@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 19:41:38 by ggispert          #+#    #+#             */
+/*   Updated: 2023/04/29 12:34:16 by ggispert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dlen;
+	size_t	slen;
+
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	if (dstsize <= dlen)
+		return (dstsize + slen);
+	ft_strlcpy(dst + dlen, src, dstsize - dlen);
+	return (dlen + slen);
+}
