46480e9dd4dbf7bd9eb16624c653ead9f5c84c67
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2024-01-31 09:38:07 +0100

    1st commit

---
+++ fract-ol/fract-ol.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract-ol.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggispert <ggispert@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 00:25:16 by ggispert          #+#    #+#             */
+/*   Updated: 2024/01/31 00:29:06 by ggispert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_OL_H
+# define FRACT_OL_H
+# include "../minilibx-linux/mlx.h"
+// # include "../minilibx_opengl_20191021/mlx.h"
+# include "../libft/libft.h"
+# include <math.h>
+
+int	main(int argc, char **argv);
+
+#endif
