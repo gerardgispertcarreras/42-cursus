c7d8dfbc035c94ccb9cfacc021287de109f0704a
Gerard Gispert Carreras <ggispert@cbr9s4.42barcelona.com>
2024-02-17 16:02:54 +0100

    update

--- pipex/srcs/error_utils.c
+++ pipex/srcs/error_utils.c
@@ -6,7 +6,7 @@
 /*   By: ggispert <ggispert@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 23:53:59 by ggispert          #+#    #+#             */
-/*   Updated: 2024/02/02 15:06:25 by ggispert         ###   ########.fr       */
+/*   Updated: 2024/02/02 16:35:41 by ggispert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,7 @@ int	_open(char *file, char wr)
 	int	fd;
 
 	if (wr)
-	{
 		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (access(file, W_OK))
-			ft_error(2, PIPEX, file);
-	}
 	else
 		fd = open(file, O_RDONLY);
 	if (fd == -1)
@@ -46,6 +42,7 @@ void	_close(int fd)
 
 void	ft_error(int exit_code, char *source, char *info)
 {
+	(void)exit_code;
 	ft_putstr_fd(source, 2);
 	perror(info);
 	exit(exit_code);
